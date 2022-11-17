b8d3b1ef4a10b0df809dbb5fafdeb08dbc5d101b
curelycue <eduard02stoica@gmail.com>
2022-11-17 14:11:56 +0200

    chakra

--- ph-trophies/src/MainMint.js
+++ ph-trophies/src/MainMint.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { ethers, BigNumber } from 'ethers';
+import { Box, Button, Flex, Input, Text } from "@chakra-ui/react";
 import TrophyMinter from './TrophyMinter.json';
 
 const TrophyMinterAddress = "";
@@ -26,24 +27,56 @@ const MainMint = ({ accounts, setAccounts }) => {
     }
 
     return (
-        <div>
-            <h1>Evolvable Prop House Trophies</h1>
-            <p>Mint your Trophy and evolve it by submiting your results now.</p>
+        <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
+        <Box width="520px">
+            <div>
+            <Text fontSize="48px" textShadow="0 5px #000000">Evolvable Prop House Trophies</Text>
+            <Text
+                fontSize="30px"
+                letterSpacing="-5.5%"
+                fontFamily="VT323"
+                textShadow="0 2px 2px #000000"
+                >
+                Mint your Trophy and evolve it by submiting your results now.
+                </Text>
+            </div>
             {isConnected ? (
                 <div>
                     <div>
                         <div>
                         <button>Check if you are Eligibille</button>
                         </div>
-                        <button onClick={handleMint}>Mint Now</button>
+                        <Button
+                        backgroundColor="#D6517D"
+                        borderRadius="5px"
+                        boxShadow="0px 2px 2px 1px #0F0F0F"
+                        color="white"
+                        cursor="pointer"
+                        fontFamily="inherit"
+                        padding="15px"
+                        marginTop="10px"
+                        onClick={handleMint}
+                        >
+                            Mint Now
+                            </Button>
                     </div>
                     <button>Evolve your Trophy</button>
                 </div>
             ) : (
-                <p>You must be connected to Mint.
-                You must be a Prop House winner to Mint.</p>
+                <Text
+                    marginTop="70px"
+                    fontSize="30px"
+                    letterSpacing="-5.5%"
+                    fontFamily="VT323"
+                    textShadow="0 3px #000000"
+                    color="#D6517D"
+                >
+                    You must be connected to Mint.
+                You must be a Prop House winner to Mint.
+                </Text>
             )}
-        </div>
+        </Box>
+        </Flex>
     );
 };
 
